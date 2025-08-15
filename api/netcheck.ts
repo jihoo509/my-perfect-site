42f2c3daf35649ecd2abe8cac2d97ec718758d09
jihoo509 <inserr509@gmail.com>
2025-08-15 21:15:27 +0900

    로로로

--- api/netcheck.ts
+++ api/netcheck.ts
@@ -1,7 +1,7 @@
 // api/netcheck.ts
 export const config = { runtime: 'nodejs' };
 
-export default async function handler(req: Request) {
+export default function handler() {
   return new Response(JSON.stringify({ ok: true, path: '/api/netcheck' }), {
     status: 200,
     headers: { 'Content-Type': 'application/json; charset=utf-8' },
